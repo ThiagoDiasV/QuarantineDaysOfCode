abf19b33d6c065dcd9295c227a486969d194b8b4
ThiagoDiasV <thiago76ers@gmail.com>
2020-04-10 17:44:58 -0300

    commit D17

--- char-words-counter/main.js
+++ char-words-counter/main.js
@@ -4,8 +4,21 @@ const textWithSpaces = document.getElementById('with-spaces');
 const textWords = document.getElementById('words');
 const textLines = document.getElementById('lines');
 
+function countWords(text) {
+  var textWithouSpacesArray = text.trim().split(/\s+/);
+  var totalWords = 0;
+  var regex = /\w+/;
+  textWithouSpacesArray.forEach((element) => {
+    if (regex.test(element)) {
+      totalWords += 1;
+    }
+  });
+  return totalWords;
+}
+
 text.addEventListener('input', function (event) {
   textWithoutSpaces.innerText = text.value.trim().split(' ').join('').length;
   textWithSpaces.innerText = text.value.trim().length;
+  textWords.innerText = countWords(text.value);
   textLines.innerText = text.value.trim().split('\n').length;
 });
