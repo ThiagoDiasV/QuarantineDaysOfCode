063b61f69ceca27ce0cd68628902b61d104dee3d
ThiagoDiasV <thiago76ers@gmail.com>
2020-04-12 19:25:02 -0300

    commit D19

--- covid/main.py
+++ covid/main.py
@@ -3,6 +3,8 @@
 # Imports necessários
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+import requests
+from datetime import datetime
 
 # Criando nosso app
 app = Flask(__name__)
@@ -13,8 +15,15 @@ Bootstrap(app)
 # Criando nossa primeira rota
 @app.route('/')
 def index():
-    doenca = "Varíola"
-    return render_template('index.html', doenca=doenca)  
+    # Fazendo a requisição para a API
+    request = requests.get('https://api.covid19.finspect.me/brcovid19/map')
+
+    # Atribuindo o JSON recebido a uma variável
+
+    # Pegando a data atual
+    date = datetime.now().strftime(format="%d/%m/%Y")
+    states_data = request.json()
+    return render_template('index.html', states=states_data, date=date)  
 
 
 # Rodando nosso app
