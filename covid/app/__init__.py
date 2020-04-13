88d7a76d9a8fa66a5c0432e935d0117dda702535
ThiagoDiasV <thiago76ers@gmail.com>
2020-04-13 17:01:27 -0300

    deploy heroku

--- covid/app/__init__.py
+++ covid/app/__init__.py
@@ -2,9 +2,11 @@
 
 # Imports necessários
 from flask import Flask
+from config import Config
 
 # Criando nosso app
 app = Flask(__name__)
+app.config.from_object(Config)
 
 from app import routes
 
