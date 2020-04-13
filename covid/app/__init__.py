11e5c26e11dc3e2d1d33ad9f774c0896362cc9d9
ThiagoDiasV <thiago76ers@gmail.com>
2020-04-13 16:55:55 -0300

    deploy heroku

---
+++ covid/app/__init__.py
@@ -0,0 +1,11 @@
+# main.py
+
+# Imports necessários
+from flask import Flask
+
+# Criando nosso app
+app = Flask(__name__)
+
+from app import routes
+
+
---
+++ covid/app/routes.py
@@ -0,0 +1,18 @@
+from app import app
+from flask import render_template
+import requests
+from datetime import datetime
+
+# Criando nossa primeira rota
+@app.route('/')
+def index():
+    # Fazendo a requisição para a API
+    request = requests.get('https://api.covid19.finspect.me/brcovid19/map')
+
+    # Atribuindo o JSON recebido a uma variável
+    states_data = request.json()
+
+    # Pegando a data atual
+    date = datetime.now().strftime(format="%d/%m/%Y")
+
+    return render_template('index.html', states=states_data, date=date)  
